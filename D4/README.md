2e5e0eaaa44cba376b9e031753ba1734b7329299
atha <giannisj3@gmail.com>
2025-03-17 15:41:09 +0200

    Update transfer SUI exercise

--- D4/README.md
+++ D4/README.md
@@ -1,6 +1,6 @@
 ## Sui & Move Bootcamp <> Programmable Transactions
 
-This exercise contains a simple Node project using Typescript, that builds transaction blocks on the Sui blockchain, and profiles their gas usage.
+This exercise contains a simple Node project using Typescript, that executes a simple transfer transaction block on the Sui blockchain.
 
 On Sui, <i>[Programmable Transaction Blocks](https://docs.sui.io/concepts/transactions/prog-txn-blocks) are a lightweight and flexible way to allow a user to execute multiple commands (eg call multiple Move functions, manage their objects, and manage their coins) in a single transaction.</i>
 
@@ -15,4 +15,7 @@ npm i
 npm run test
 ```
 
-Fill in the `src/tests/transferSUI.test.ts` test, so that it builds and dry runs a transaction for transfering a specific SUI amount to a recipient addresses.
+### Instructions
+
+- The transferSui.test.ts test is calling the function defined in the `src/helpers/transferSUI.ts`, to transfer a specified amount of SUI tokens to a specified recipient address.
+- Fill in the function, so that the test passes.
--- D4/ptbs/src/tests/transferSUI.test.ts
+++ D4/ptbs/src/tests/transferSUI.test.ts
@@ -1,41 +1,19 @@
-import { Transaction } from "@mysten/sui/transactions";
-import { MIST_PER_SUI, SUI_TYPE_ARG } from "@mysten/sui/utils";
+import { MIST_PER_SUI } from "@mysten/sui/utils";
 import { ENV } from "../env";
-import { suiClient } from "../suiClient";
-import { getAddress } from "../helpers/getAddress";
+import { transferSUI } from "../helpers/transferSUI";
 
-test("Dry run transferring 0.01 SUI to the RECIPIENT_ADDRESS", async () => {
+test("Transferring 0.01 SUI to the RECIPIENT_ADDRESS", async () => {
   const AMOUNT = 0.01 * Number(MIST_PER_SUI);
-  const senderAddress = getAddress({ secretKey: ENV.USER_SECRET_KEY });
 
-  // Create a transaction for transferring AMOUNT SUI to the RECIPIENT_ADDRESS
-  const tx = new Transaction();
-  tx.setSender(senderAddress);
-  const [coin] = tx.splitCoins(tx.gas, [AMOUNT]);
-  tx.transferObjects([coin], ENV.RECIPIENT_ADDRESS);
-
-  // build and dry-run the transaction
-  const txBytes = await tx.build({ client: suiClient });
-  const { effects, balanceChanges } = await suiClient.dryRunTransactionBlock({
-    transactionBlock: txBytes,
-  });
-  const recipientBalanceChange = balanceChanges.find(({ owner }) => {
-    const addressOwner = (owner as { AddressOwner: string }).AddressOwner;
-    return addressOwner === ENV.RECIPIENT_ADDRESS;
-  });
-  const senderBalanceChange = balanceChanges.find(({ owner }) => {
-    const addressOwner = (owner as { AddressOwner: string }).AddressOwner;
-    return addressOwner === senderAddress;
-  });
-
-  // log the gas cost and transaction digest
-  // console.log("Gas cost:", effects.gasUsed);
-  // console.log("Digest:", effects.transactionDigest);
+  const { status, senderSUIBalanceChange, recipientSUIBalanceChange } =
+    await transferSUI({
+      amount: AMOUNT,
+      senderSecretKey: ENV.USER_SECRET_KEY,
+      recipientAddress: ENV.RECIPIENT_ADDRESS,
+    });
 
   // expect the transaction to be successful and the balance changes to have changed accordingly
-  expect(effects.status.status).toBe("success");
-  expect(recipientBalanceChange?.coinType).toBe(SUI_TYPE_ARG);
-  expect(parseInt(recipientBalanceChange!.amount)).toBe(AMOUNT);
-  expect(senderBalanceChange?.coinType).toBe(SUI_TYPE_ARG);
-  expect(parseInt(senderBalanceChange!.amount)).toBeLessThan(-AMOUNT);
+  expect(status).toBe("success");
+  expect(recipientSUIBalanceChange).toBe(AMOUNT);
+  expect(senderSUIBalanceChange).toBeLessThan(-AMOUNT);
 });
