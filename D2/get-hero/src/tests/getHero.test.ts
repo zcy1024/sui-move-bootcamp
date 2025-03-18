f1ca4a7f957e3b02f8be47156225a56fe7be34b8
atha <giannisj3@gmail.com>
2025-03-18 14:50:12 +0200

    Modify hero package

--- D2/get-hero/src/tests/getHero.test.ts
+++ D2/get-hero/src/tests/getHero.test.ts
@@ -2,9 +2,10 @@ import { SuiObjectResponse } from "@mysten/sui/client";
 import { getHero } from "../helpers/getHero";
 import { Hero, parseHeroContent } from "../helpers/parseHeroContent";
 import { ENV } from "../env";
+import { getHeroSwordIds } from "../helpers/getHeroSwordIds";
 
 const HERO_OBJECT_ID =
-  "0x66791f5e262d5a92fb26120afa6bd11ba84b9ac0cb7fecdbbed0565238b1e02f";
+  "0x5b89d98e0b73963c23bba7e877d9ebe066bf793277242dcc887b5b9d9b6d74cd";
 
 describe("Get Hero", () => {
   let objectResponse: SuiObjectResponse;
@@ -16,19 +17,18 @@ describe("Get Hero", () => {
   test("Hero Exists", () => {
     expect(objectResponse.data).toBeDefined();
     expect(objectResponse.data!.objectId).toBeDefined();
-    expect(objectResponse.data!.type).toBe(
-      `${ENV.PACKAGE_ID}::basic_move::Hero`
-    );
+    expect(objectResponse.data!.type).toBe(`${ENV.PACKAGE_ID}::hero::Hero`);
   });
 
   test("Hero Content", () => {
     const hero: Hero = parseHeroContent(objectResponse);
     expect(hero.id).toBe(HERO_OBJECT_ID);
-    expect(hero.stamina).toBeNull();
-    expect(hero.category).toBeNull();
-    expect(hero.name).toBeDefined();
-    expect(hero.weapon).toBeDefined();
-    expect(hero.weapon!.name).toBeDefined();
-    expect(hero.weapon!.destruction_power).toBeDefined();
+    expect(hero.stamina).toBeDefined();
+    expect(hero.health).toBeDefined();
+  });
+
+  test("Hero has attached swords", async () => {
+    const swordIds = await getHeroSwordIds(objectResponse.data!.objectId);
+    expect(swordIds.length).toBeGreaterThan(0);
   });
 });
