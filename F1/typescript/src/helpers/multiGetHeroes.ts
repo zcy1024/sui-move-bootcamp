229b9011f117e7915b7280fb09ed4c87e2bb510f
atha <giannisj3@gmail.com>
2025-04-04 14:09:56 +0300

    Delete redundant helper method in TS scripts

--- F1/typescript/src/helpers/multiGetHeroes.ts
+++
@@ -1,7 +0,0 @@
-import { suiClient } from "../suiClient";
-
-/**
- * Receives an array of heroes ids and returns an array with the heroes
- * We can ignore the pagination for this function, and keep only the first 50 Ids.
- */
-export const multiGetHeroes = async (heroesIds: string[]) => {};
