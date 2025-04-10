e6e5cb205c566d2e5093c20623209052dbbb5050
nikos-terzo <nikos.terzo@mystenlabs.com>
2025-04-11 00:40:12 +0300

    [I2]: Minor: Remove unused const

--- I2/publish/src/consts.ts
+++ I2/publish/src/consts.ts
@@ -15,8 +15,6 @@ function getKeypair(parsed: ParsedKeypair) {
 }
 dotenv.config();
 
-export const RECIPIENT="0x69628f2cf992e5749022060a25feb471d96b97e2a518243136e6f4b0b121bc9a";
-
 const privKey = process.env.ADMIN_PRIVATE_KEY!;
 const parsed = decodeSuiPrivateKey(privKey);
 export const ADMIN_KEYPAIR: Keypair = getKeypair(parsed);
